5b9f50ceab88ba1e05f463de1cd86fb440f5aca3
Thomas Prosser <tp@thomit.com>
2020-07-22 16:20:40 +0200

    various changes to plugin and child theme

--- wordpress/wp-content/plugins/cz-gallery/cz-gallery.php
+++ wordpress/wp-content/plugins/cz-gallery/cz-gallery.php
@@ -8,6 +8,24 @@
 
 /* Start Adding Functions Below this Line */
 
+/**
+ * Remove old Sparkling Theme Bootstrap
+ */
+function cz_remove_old_bootstrap(){
+  wp_dequeue_style('sparkling-bootstrap');
+  wp_dequeue_script('sparkling-bootstrapjs');
+}
+add_action('wp_print_styles', 'cz_remove_old_bootstrap');
+
+/**
+ * Add shiny new Bootstrap
+ */
+function cz_add_new_bootstrap(){
+  wp_enqueue_style('cz_bootstrap_version', plugin_dir_url(__FILE__).'css/bootstrap.min.css');
+  wp_enqueue_script('cz_bootstrap_scripts', plugin_dir_url(__FILE__).'js/bootstrap.bundle.min.js');
+}
+add_action('wp_enqueue_scripts', 'cz_add_new_bootstrap');
+
 function create_posttype() {
 
   register_post_type( 'portfolio_item',
--- wordpress/wp-content/themes/sparkling/single-portfolio_item.php
+++ wordpress/wp-content/themes/sparkling/single-portfolio_item.php
@@ -9,15 +9,16 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+          <?php implode(',',get_the_category())?>
 
-		<?php
+
+            <?php
 
         $args = array( 'post_type' => 'portfolio_item');
         $the_query = new WP_Query( $args );
         if($the_query->have_posts())  :
             while ( $the_query->have_posts() ) :
                 $the_query->the_post(); ?>
-
                 <h2><?php the_title();?></h2>
                 <?php the_content();?>
                 <p><?php the_field('beschreibung');?></p>
