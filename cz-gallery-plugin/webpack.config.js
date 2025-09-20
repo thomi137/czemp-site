1629fc5c786e7ae3aaa44a94d26a01bb84bee969
Thomas Prosser <tp@thomit.com>
2025-09-20 16:28:24 +0200

    Changed to block theme Twenty Twenty Five and base a child theme

--- cz-gallery-plugin/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './src/js/cz-gallery-plugin.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'js'),
-    },
-};
