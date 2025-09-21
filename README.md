21394e2b0c514a81032b84ce41bb4a266e138396
Thomas Prosser <tp@thomit.com>
2025-09-21 19:15:12 +0200

    Expanded on Readme

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # Software for WordPress czemp Theme
+
+## Introduction
+This is a repository for a WordPress block theme and possibly plugins facilitating a more efficient upload functionality 
+for a custom made home site. The theme is currently based on the WordPress Twenty Twenty Five theme, and is as such a 
+[Child Theme](https://developer.wordpress.org/themes/advanced-topics/child-themes/) currently. It is hosted here so that
+the developer has a means of safely securing the code, but feel free to fork if you find it useful for your intents, provided
+that the GPLv2 Licence is adhered to.
+
+## Components
+
+### Development Environment
+
+Normally, the development setup for WordPress suggests to use some distribution
+of a virtual machine containing the full stack of DB, WordPress installation and a Webserver.
+because the author felt that it leads to more flexibility, he chose to use a dockerized setup and docker compose, as documented
+in the `docker-compose.yml` file. 
+
+#### Images
+The current `docker-compose.yml` uses standard [Dockerhub](https://hub.docker.com/) images, specifically:
+
+* [Mysql](https://hub.docker.com/_/mysql)
+* [PhpMyAdmin](https://hub.docker.com/_/phpmyadmin)
+* [WordPress](https://hub.docker.com/_/wordpress)
+
+Note that you should configure images according to your target environment. It may be even worthwhile to create you own `Dockerfile`s. 
+Any improvement and comment on this setup is greatly appreciated (as are corresponding PRs, BTW 😉).
+
+#### Startup
+
+Out of the box, this is easy:
+
+    docker-compose up -d
+
+and 
+
+    docker-compose down
+
+to get rid of it.
+
+#### Usage
+
+Note that there are some volumes linked to the local hard drive. This is so that changes in the current code
+are directly linked into the docker container. Although this is very convenient, it does only provide some sort of poor man's
+hot reload in that the browser needs to be refreshed manually.
+Since the author sees that as a minor inconvenience, he did not further fine tune.
+
+However, again, any PR or Email to the [author](mailto:thomas@rosser.ch) on how to improve is greatly appreciated.
+
+## Theme Files
+
+WIP
+
--- cz-theme/patterns/footer.php
+++ cz-theme/patterns/footer.php
@@ -10,48 +10,53 @@
  * @since 1.0
  */
 ?>
-
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
-	<div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"grid"}} -->
-		<div class="wp-block-group"><!-- wp:group {"style":{"layout":{"columnSpan":3,"rowSpan":1}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
-			<div class="wp-block-group"><!-- wp:site-logo /-->
+    <div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"grid","minimumColumnWidth":null,"columnCount":6}} -->
+        <div class="wp-block-group"><!-- wp:group {"style":{"layout":{"columnSpan":3,"rowSpan":1}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+            <div class="wp-block-group"><!-- wp:group {"style":{"layout":{"columnSpan":3,"rowSpan":1}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+                <div class="wp-block-group"><!-- wp:site-logo /-->
 
-				<!-- wp:site-title {"level":2,"style":{"layout":{"columnSpan":2,"rowSpan":1}}} /--></div>
-			<!-- /wp:group -->
+                    <!-- wp:site-title {"level":2,"style":{"layout":{"columnSpan":2,"rowSpan":1}}} /--></div>
+                <!-- /wp:group --></div>
+            <!-- /wp:group -->
 
-			<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|80"},"layout":{"columnSpan":2,"rowSpan":1}},"layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"top","justifyContent":"right"}} -->
-			<div class="wp-block-group"><!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
-				<!-- wp:navigation-link {"label":"Blog","url":"#"} /-->
+            <!-- wp:spacer -->
+            <div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+            <!-- /wp:spacer -->
 
-				<!-- wp:navigation-link {"label":"About","url":"#"} /-->
+            <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|80"},"layout":{"columnSpan":2,"rowSpan":1}},"layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"top","justifyContent":"right"}} -->
+            <div class="wp-block-group"><!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
+                <!-- wp:navigation-link {"label":"Blog","url":"#"} /-->
 
-				<!-- wp:navigation-link {"label":"FAQs","url":"#"} /-->
+                <!-- wp:navigation-link {"label":"About","url":"#"} /-->
 
-				<!-- wp:navigation-link {"label":"Authors","url":"#"} /-->
-				<!-- /wp:navigation -->
+                <!-- wp:navigation-link {"label":"FAQs","url":"#"} /-->
 
-				<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
-				<!-- wp:navigation-link {"label":"Events","url":"#"} /-->
+                <!-- wp:navigation-link {"label":"Authors","url":"#"} /-->
+                <!-- /wp:navigation -->
 
-				<!-- wp:navigation-link {"label":"Shop","url":"#"} /-->
+                <!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} -->
+                <!-- wp:navigation-link {"label":"Events","url":"#"} /-->
 
-				<!-- wp:navigation-link {"label":"Patterns","url":"#"} /-->
+                <!-- wp:navigation-link {"label":"Shop","url":"#"} /-->
 
-				<!-- wp:navigation-link {"label":"Themes","url":"#"} /-->
-				<!-- /wp:navigation --></div>
-			<!-- /wp:group --></div>
-		<!-- /wp:group -->
+                <!-- wp:navigation-link {"label":"Patterns","url":"#"} /-->
 
-		<!-- wp:spacer {"height":"var:preset|spacing|70"} -->
-		<div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
-		<!-- /wp:spacer -->
+                <!-- wp:navigation-link {"label":"Themes","url":"#"} /-->
+                <!-- /wp:navigation --></div>
+            <!-- /wp:group --></div>
+        <!-- /wp:group -->
 
-		<!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
-		<div class="wp-block-group alignfull"><!-- wp:paragraph {"fontSize":"small"} -->
-			<p class="has-medium-font-size">
+        <!-- wp:spacer {"height":"var:preset|spacing|40"} -->
+        <div style="height:var(--wp--preset--spacing--40)" aria-hidden="true" class="wp-block-spacer"></div>
+        <!-- /wp:spacer -->
+
+        <!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+        <div class="wp-block-group alignfull"><!-- wp:paragraph {"className":"has-medium-font-size","fontSize":"medium"} -->
+            <p class="has-medium-font-size">
                 &copy; <?php echo date('Y') ?> Claudia Zemp</p>
-			<!-- /wp:paragraph --></div>
-		<!-- /wp:group --></div>
-	<!-- /wp:group --></div>
+            <!-- /wp:paragraph --></div>
+        <!-- /wp:group --></div>
+    <!-- /wp:group --></div>
 <!-- /wp:group -->
