64ff9c9f48e256ba82fba5092438fc69f8e5d007
Thomas Prosser <tp@thomit.com>
2026-07-04 22:41:17 +0200

    Somme final touches, we leave the menu and mobile for now.

--- cz-theme/patterns/header.php
+++ cz-theme/patterns/header.php
@@ -4,7 +4,7 @@
  * Slug: czemp/header
  * Categories: header
  * Block Types: core/template-part/header
- * Description: Site header with logo.
+ * Description: Site header with logo and site title.
  *
  * @author Thomas Prosser
  * @package czemp
@@ -15,11 +15,19 @@
 <!-- wp:group {"tagName":"header","className":"cz-header","layout":{"type":"constrained"}} -->
 <header class="wp-block-group cz-header">
 
-    <!-- wp:image {"url":"\/wp-content\/themes\/cz-theme\/assets\/images\/cz_Logo_without_text.webp","alt":"Claudia Zemp","width":80,"sizeSlug":"full"} -->
-    <figure class="wp-block-image size-full">
-        <img src="/wp-content/themes/cz-theme/assets/images/cz_Logo_without_text.webp" alt="Claudia Zemp" width="80"/>
-    </figure>
-    <!-- /wp:image -->
+    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center","justifyContent":"left"},"style":{"spacing":{"blockGap":"16px"}}} -->
+    <div class="wp-block-group" style="gap:16px">
+
+        <!-- wp:image {"url":"\/wp-content\/themes\/cz-theme\/assets\/images\/cz_Logo_without_text.webp","alt":"Claudia Zemp","width":72,"sizeSlug":"full","linkDestination":"custom","href":"/"} -->
+        <figure class="wp-block-image size-full">
+            <a href="/"><img src="/wp-content/themes/cz-theme/assets/images/cz_Logo_without_text.webp" alt="Claudia Zemp" width="72"/></a>
+        </figure>
+        <!-- /wp:image -->
+
+        <!-- wp:site-title {"style":{"typography":{"fontWeight":"300","letterSpacing":"0.08em","textTransform":"uppercase"},"elements":{"link":{"color":{"text":"#000000"},":hover":{"color":{"text":"#000000"}}}}}} /-->
+
+    </div>
+    <!-- /wp:group -->
 
 </header>
 <!-- /wp:group -->
